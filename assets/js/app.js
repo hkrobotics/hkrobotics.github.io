ec26740484821120de737e0ddc597c92f806e786
hkrobotics <kumarhemant24jan@gmail.com>
2021-09-21 14:35:56 +0530

    initial commit

---
+++ assets/js/app.js
@@ -0,0 +1,14 @@
+$(function() {
+    var text = $(".text");
+    $(window).scroll(function() {
+      var scroll = $(window).scrollTop();
+  
+      if (scroll <= 200 ) {
+          
+        text.removeClass("hidden");
+      } else {
+        text.addClass("hidden");
+      }
+    });
+  });
+  
