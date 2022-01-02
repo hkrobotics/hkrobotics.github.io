a7795242db1908ce494f43dc8553a44c9da40f55
hkrobotics <kumarhemant24jan@gmail.com>
2022-01-02 18:41:49 +0530

    🚀 Feature Update : Add Scroll Reveal JS

---
+++ assets/js/scroll.js
@@ -0,0 +1,72 @@
+window.sr = ScrollReveal();
+        sr.reveal('.hero_content', {
+            duration: 2000,
+            origin: 'bottom',
+            distance: '100px',
+            easing: 'ease-in-out',
+            scale: 1,
+            opacity: 0
+        });
+
+        sr.reveal('.hero_img', {
+            duration: 2000,
+            origin: 'bottom',
+            distance: '100px',
+            easing: 'ease-in-out',
+            scale: 1,
+            opacity: 0
+        });
+
+        sr.reveal('.intro_wrapper', {
+            duration: 2000,
+            origin: 'bottom',
+            distance: '100px',
+            easing: 'ease-in-out',
+            scale: 1,
+            opacity: 0
+        });
+
+        sr.reveal('.button_wrapper', {
+            duration: 2000,
+            origin: 'bottom',
+            distance: '100px',
+            easing: 'ease-in-out',
+            scale: 1,
+            opacity: 0
+        });
+
+        sr.reveal('.typed_wrapper', {
+            duration: 2000,
+            origin: 'bottom',
+            distance: '100px',
+            easing: 'ease-in-out',
+            scale: 1,
+            opacity: 0
+        });
+
+        sr.reveal('.main_credits', {
+            duration: 2000,
+            origin: 'bottom',
+            distance: '100px',
+            easing: 'ease-in-out',
+            scale: 1,
+            opacity: 0
+        });
+
+        sr.reveal('.sub_credit', {
+            duration: 2000,
+            origin: 'bottom',
+            distance: '100px',
+            easing: 'ease-in-out',
+            scale: 1,
+            opacity: 0
+        });
+
+        sr.reveal('.socials_list li', {
+            duration: 2000,
+            origin: 'bottom',
+            distance: '100px',
+            easing: 'ease-in-out',
+            scale: 1,
+            opacity: 0
+        });
