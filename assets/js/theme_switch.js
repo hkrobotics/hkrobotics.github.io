5dc04e31a8d1f4d7c82a88f53242f71bf260809a
hkrobotics <kumarhemant24jan@gmail.com>
2022-01-06 19:35:35 +0530

    🚀 Feature Update : Dark Mode [Beta]

---
+++ assets/js/theme_switch.js
@@ -0,0 +1,19 @@
+var checkbox = document.querySelector('input[name=theme_switch_chk]')
+var mode = localStorage.getItem('mode')
+
+if (mode == 'dark') {
+    document.documentElement.setAttribute('data-theme', 'dark')
+    checkbox.checked = true
+}
+
+checkbox.addEventListener('change', function () {
+    if (this.checked) {
+      
+        document.documentElement.setAttribute('data-theme', 'dark')
+        localStorage.setItem('mode', 'dark')
+    } else {
+      
+        document.documentElement.setAttribute('data-theme', 'light')
+        localStorage.setItem('mode', 'light')
+    }
+})
