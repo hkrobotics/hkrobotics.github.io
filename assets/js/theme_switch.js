91f6534b7f7d3af5210f23630b4e4d0346244db7
hkrobotics <kumarhemant24jan@gmail.com>
2022-01-18 21:05:03 +0530

    🔃 UI Refresh Part 2

--- assets/js/theme_switch.js
+++ assets/js/theme_switch.js
@@ -20,24 +20,5 @@ checkbox.addEventListener('change', function () {
 
     
 
-    // var myImg = document.getElementById('hero_img');
-    // console.log(myImg.getAttribute('src'));
-    // if(mode == 'dark'){
-    //     refreshImage('hero_img', './assets/images/hero_img_dark.png');
-    // }
-    // else{
-    //     refreshImage('hero_img', './assets/images/hero_img_light.png');
-    // }
-
-    // function refreshImage(imgElement, imgURL){    
-    //     // create a new timestamp 
-    //     var timestamp = new Date().getTime();  
-      
-    //     var el = document.getElementById(imgElement);  
-      
-    //     var queryString = "?t=" + timestamp;    
-       
-    //     el.src = imgURL + queryString;    
-    // }  
 
 
