d4ef1e9be6cdb327ded4e8b53abdbca97125ee1a
hkrobotics <kumarhemant24jan@gmail.com>
2022-01-28 09:36:47 +0530

    🆕 Add New Projects Section

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -1,133 +1,190 @@
 window.sr = ScrollReveal();
 
-sr.reveal('.hero div', {
-            duration: 3000,
-            origin: 'bottom',
-            distance: '100px',
-            easing: 'ease-in-out',
-            scale: 1,
-            opacity: 0,
-            reveal: true,
-            delay: 0,
-            reset: true,
-            mobile: true,
-            viewFactor: 0.5
-        });
-        
-        sr.reveal('.hero_content div', {
-            duration: 2000,
-            interval: 200,
-            origin: 'bottom',
-            distance: '100px',
-            easing: 'ease-in-out',
-            scale: 1,
-            opacity: 0,
-            reveal: true,
-            delay: 500,
-            reset: false,
-            mobile: true,
-            viewFactor: 0.5
+sr.reveal(".hklogo", {
+    duration: 500,
+    interval: 200,
+    origin: "top",
+    distance: "32px",
+    easing: "ease-in-out",
+    scale: 1,
+    opacity: 0,
+    reset: true,
+    mobile: false,
+    delay: 0,
+  });
 
-        });
 
-        sr.reveal('.hero_img div', {
-            duration: 3000,
-            interval: 50,
-            origin: 'bottom',
-            distance: '100px',
-            easing: 'ease-in-out',
-            scale: 1,
-            opacity: 0,
-            reveal: true,
-            delay: 0,
-            reset: true,
-            mobile: true,
-            viewFactor: 0.5
-        });
-        
+sr.reveal(".nav_list li", {
+    duration: 500,
+    interval: 200,
+    origin: "bottom",
+    distance: "32px",
+    easing: "ease-in-out",
+    scale: 1,
+    opacity: 0,
+    // reset: true,
+    mobile: false,
+    delay: 0,
+  });
 
-        sr.reveal('.intro_wrapper', {
-            duration: 2000,
-            origin: 'bottom',
-            distance: '100px',
-            easing: 'ease-in-out',
-            scale: 1,
-            opacity: 0
-        });
 
-        sr.reveal('.button_wrapper', {
-            duration: 2000,
-            origin: 'bottom',
-            distance: '100px',
-            easing: 'ease-in-out',
-            scale: 1,
-            opacity: 0
-        });
 
-        sr.reveal('.typed_wrapper', {
-            duration: 2000,
-            origin: 'bottom',
-            distance: '100px',
-            easing: 'ease-in-out',
-            scale: 1,
-            opacity: 0
-        });
+sr.reveal(".hero div", {
+  duration: 800,
+  interval:100,
+  origin: "top",
+  distance: "100px",
+  easing: "ease-in-out",
+  scale: 1,
+  opacity: 0,
+  reveal: true,
+  delay: 100,
+  reset: true,
+  mobile: true,
+  viewFactor: 0.5
+});
 
-        sr.reveal('.main_credits', {
-            interval: 200,
-            duration: 2000,
-            origin: 'bottom',
-            distance: '96px',
-            easing: 'ease-in-out',
-            scale: 1,
-            opacity: 0,
-            reset: true
-        });
 
-        sr.reveal('.sub_credit', {
-            interval: 200,
-            duration: 1500,
-            origin: 'bottom',
-            distance: '64px',
-            easing: 'ease-in-out',
-            scale: 1,
-            opacity: 0,
-            reset: true
-        });
+// sr.reveal(".hero_content div", {
+//   duration: 2000,
+//   interval: 200,
+//   origin: "bottom",
+//   distance: "100px",
+//   easing: "ease-in-out",
+//   scale: 1,
+//   opacity: 0,
+//   reveal: true,
+//   delay: 500,
+//   reset: false,
+//   mobile: true,
+//   viewFactor: 0.5,
+// });
 
-        sr.reveal('.socials_list li', {
-            duration: 500,
-            interval: 200,
-            origin: 'bottom',
-            distance: '32px',
-            easing: 'ease-in-out',
-            scale: 1,
-            opacity: 0,
-            reset: true
-        });
+// sr.reveal(".hero_img div", {
+//   duration: 3000,
+//   interval: 50,
+//   origin: "bottom",
+//   distance: "100px",
+//   easing: "ease-in-out",
+//   scale: 1,
+//   opacity: 0,
+//   reveal: true,
+//   delay: 0,
+//   reset: true,
+//   mobile: true,
+//   viewFactor: 0.5,
+// });
 
-        
+// sr.reveal(".intro_wrapper", {
+//   duration: 2000,
+//   origin: "bottom",
+//   distance: "100px",
+//   easing: "ease-in-out",
+//   scale: 1,
+//   opacity: 0,
+// });
 
-        sr.reveal('.skills', {
-            duration: 500,
-            interval: 100,
-            origin: 'bottom',
-            distance: '32px',
-            easing: 'ease-in-out',
-            scale: 1,
-            opacity: 0,
-            reset: true
-        });
+// sr.reveal(".button_wrapper", {
+//   duration: 2000,
+//   origin: "bottom",
+//   distance: "100px",
+//   easing: "ease-in-out",
+//   scale: 1,
+//   opacity: 0,
+// });
 
-        sr.reveal('.skills-container div', {
-            duration: 500,
-            interval: 100,
-            origin: 'bottom',
-            distance: '32px',
-            easing: 'ease-in-out',
-            scale: 1,
-            opacity: 0,
-            reset: true
-        });
+// sr.reveal(".typed_wrapper", {
+//   duration: 2000,
+//   origin: "bottom",
+//   distance: "100px",
+//   easing: "ease-in-out",
+//   scale: 1,
+//   opacity: 0,
+// });
 
-        
+
+
+// Skills 🟢
+sr.reveal(".skills", {
+  duration: 500,
+  interval: 100,
+  origin: "bottom",
+  distance: "32px",
+  easing: "ease-in-out",
+  scale: 1,
+  opacity: 0,
+  reset: true,
+});
+
+sr.reveal(".skills-container div", {
+  duration: 500,
+  interval: 100,
+  origin: "bottom",
+  distance: "32px",
+  easing: "ease-in-out",
+  scale: 1,
+  opacity: 0,
+  reset: true,
+});
+
+//Projects 🟢
+
+sr.reveal(".projects-grid .projects-card-container", {
+  duration: 500,
+  interval: 100,
+  origin: "bottom",
+  distance: "32px",
+  easing: "ease-in-out",
+  scale: 1,
+  opacity: 0,
+  reset: true,
+});
+
+sr.reveal(".projects-card-container .pill", {
+  duration: 100,
+  interval: 50,
+  origin: "bottom",
+  distance: "32px",
+  easing: "ease-in-out",
+  scale: 1,
+  opacity: 0,
+  // reset: true
+});
+
+
+sr.reveal(".contact_container h2,.contact_container p,.contact_container div", {
+    duration: 500,
+    interval: 10,
+    origin: "bottom",
+    distance: "32px",
+    easing: "ease-in-out",
+    scale: 1,
+    opacity: 0,
+    reset: true,
+  });
+
+// Socials 🟢
+sr.reveal(".sub_credit", {
+    interval: 200,
+    duration: 500,
+    origin: "bottom",
+    distance: "64px",
+    easing: "ease-in-out",
+    scale: 1,
+    opacity: 0,
+    reset: true,
+    mobile: false
+  });
+  
+  sr.reveal(".socials_list li", {
+    duration: 500,
+    interval: 100,
+    origin: "bottom",
+    distance: "32px",
+    easing: "ease-in-out",
+    scale: 1,
+    opacity: 0,
+    reset: true,
+    mobile: false,
+    delay: 100,
+  });
