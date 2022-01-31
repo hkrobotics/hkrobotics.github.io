2a0564a0337dd0bb33f7a03a578775fb83d75d10
hkrobotics <kumarhemant24jan@gmail.com>
2022-01-31 13:35:33 +0530

    ⬆️ Add Preloader

---
+++ assets/js/preloader.js
@@ -0,0 +1,7 @@
+//function to get rid of preloader
+
+window.addEventListener('load', () => {
+    // const preloader = document.querySelector('.loader');
+    const loader = document.getElementById('loader');
+    loader.classList.add('fadeOut');
+});
